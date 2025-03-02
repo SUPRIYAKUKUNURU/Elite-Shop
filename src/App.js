ad7e04c21b241d889abdead9a4736762c9bbe2fe
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-03-02 12:27:04 +0530

    added details

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./Home";
 import LoginForm from "./LoginForm";
 import SignUp from "./SignUp";
 import Cart from "./Cart";
-import Details from "./Details"; // ✅ Import Details Page
+import Details from "./Details"; 
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
@@ -12,9 +12,9 @@ function App() {
   const [search, setSearch] = useState("");
   const [sort, setSort] = useState("");
   const [cart, setCart] = useState([]);
-  const [products, setProducts] = useState([]); // ✅ Global product state
+  const [products, setProducts] = useState([]); 
 
-  // ✅ Fetch products globally
+  
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
@@ -41,7 +41,7 @@ function App() {
       <Navbar setSearch={setSearch} setSort={setSort} cartCount={cart.length} />
       <Routes>
         <Route path="/" element={<Home search={search} sort={sort} addToCart={addToCart} products={products} />} />
-        <Route path="/details/:id" element={<Details products={products} />} /> {/* ✅ Fix: Added Route */}
+        <Route path="/details/:id" element={<Details products={products} />} /> 
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
